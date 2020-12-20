5614ee2eedf5e3a3dfff0c97d36e7802592155d1
Jordan <jordanhernando1085@gmail.com>
2020-12-19 22:35:07 -0300

    Oculatar formulario de tareas si no hay proyecto

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,32 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
+import { useContext } from 'react';
 
 const FormTarea = () => {
-    return (
 
-        <div className="formulario">
-            <form>
+  // Extraer si un proyecto esta activo
+  const proyectosCotext = useContext(proyectoContext); 
+  const { proyecto } = proyectosCotext; 
 
-                <div className="contenedor-input">
-                      <input
-                        type="text"
-                        className="input-text"
-                        placeholder="Nombre Tarea..."
-                        name="nombre"
-                      />
-                </div>
+  //si no hay proyecto seleccionado
+  if(!proyecto) return null;
 
-                <div className="contenedor-input">
-                      <input
-                        type="submit"
-                        className="btn btn-primario btn-block"
-                        value="Agregar Tarea"
-                      />
-                </div>
+  // Array destructuring para eextraer proyecto actual
+  const [proyectoActual] = proyecto;
 
-            </form>
-        </div> 
+  return (
 
-     );
+      <div className="formulario">
+          <form>
+
+              <div className="contenedor-input">
+                    <input
+                      type="text"
+                      className="input-text"
+                      placeholder="Nombre Tarea..."
+                      name="nombre"
+                    />
+              </div>
+
+              <div className="contenedor-input">
+                    <input
+                      type="submit"
+                      className="btn btn-primario btn-block"
+                      value="Agregar Tarea"
+                    />
+              </div>
+
+          </form>
+      </div> 
+
+    );
 }
  
 export default FormTarea;
